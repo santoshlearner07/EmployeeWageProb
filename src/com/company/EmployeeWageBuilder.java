df054f9658c46d7416082cb41b31b3621215798e
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 14:33:42 +0530

    Added Reliance and Dmart company calculated separtae wage

--- src/com/company/EmployeeWageBuilder.java
+++ src/com/company/EmployeeWageBuilder.java
@@ -1,23 +1,35 @@
 package com.company;
 
 public class EmployeeWageBuilder {
-
     //constants
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int PER_DAY_WORK_HOUR = 8;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
+
+    private final String company;
+    private final int empRatePerHr;
+    private final int numOfWorkingDays;
+    private final int maxHourPerMonth;
+
+    public EmployeeWageBuilder(String company, int empRatePerHr, int numOfWorkingDays, int maxHourPerMonth) {
+        this.company = company;
+        this.empRatePerHr = empRatePerHr;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHourPerMonth = maxHourPerMonth;
+    }
+
 
     public static void main(String[] args) {
+        EmployeeWageBuilder dMart = new EmployeeWageBuilder("Dmart", 20, 2, 10);
+        EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance", 45, 2, 9);
+
+
 //        Variables
-        int empPerDayIncome = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        int totalIncome = 0;
-        while (totalEmpHrs < MAX_HRS_IN_MONTH &&
-                totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        int empPerDayIncome = 0, relEmpPerDayIncome = 0;
+        int totalEmpHrs = 0, totalRelEmpHrs = 0;
+        int totalWorkingDays = 0, totalRelWorkingDays = 0;
+        int totalIncome = 0, totalRelIncome = 0;
+        while (totalEmpHrs < dMart.maxHourPerMonth &&
+                totalWorkingDays < dMart.numOfWorkingDays) {
             {
                 totalWorkingDays++;  //it will increment till 20
                 int empHrs = 0;
@@ -26,11 +38,9 @@ public class EmployeeWageBuilder {
                 switch (empCheck) {
                     case IS_FULL_TIME:
                         empHrs = 8;
-                        empPerDayIncome = PER_DAY_WORK_HOUR * EMP_RATE_PER_HOUR; // 160
                         break;
                     case IS_PART_TIME:
                         empHrs = 4;
-                        empPerDayIncome = (PER_DAY_WORK_HOUR * EMP_RATE_PER_HOUR) / 2;  // 80
                         break;
                     default:
                         empHrs = 0; // 0
@@ -38,13 +48,37 @@ public class EmployeeWageBuilder {
                 }
                 totalEmpHrs += empHrs; // it depends on empCheck value
                 totalIncome += empPerDayIncome;
-                int monthlyIncome = empHrs * totalWorkingDays;
+                int monthlyIncome = empHrs * dMart.empRatePerHr;
 
             }
-        }
-        System.out.println();
-        System.out.println("Total Hrs worked in a month :- " + totalEmpHrs + " Hrs");
-        System.out.println("Day " + totalWorkingDays + " earned " + totalIncome + " rs");
+            while (totalRelEmpHrs < reliance.maxHourPerMonth &&
+                    totalRelWorkingDays < reliance.numOfWorkingDays) {
+                {
+                    totalRelWorkingDays++;  //it will increment till 20
+                    int rempHrs = 0;
 
+                    int empCheck = ((int) Math.floor(Math.random() * 10) % 3);  // random value between 0,1,2 which will take u to switch case
+                    switch (empCheck) {
+                        case IS_FULL_TIME:
+                            rempHrs = 9;
+                            break;
+                        case IS_PART_TIME:
+                            rempHrs = 5;
+                            break;
+                        default:
+                            rempHrs = 0; // 0
+
+                    }
+                    totalRelEmpHrs += rempHrs; // it depends on empCheck value
+                    totalRelIncome += relEmpPerDayIncome;
+                    int monthlyIncome = rempHrs * dMart.empRatePerHr;
+                }
+            }
+            int totalEmpWage = totalEmpHrs * dMart.empRatePerHr;
+            int totalEmpWageR = totalEmpHrs * reliance.empRatePerHr;
+            System.out.println("Total Emp Wage for " + dMart.company + " is :- " + totalEmpWage);
+            System.out.println("Total Emp Wage for " + reliance.company + " is :- " + totalEmpWageR);
+
+        }
     }
 }
