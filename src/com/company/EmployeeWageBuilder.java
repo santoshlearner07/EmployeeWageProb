f3986c6058f4ed36167db700c1fae6f11f151b32
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 12:38:33 +0530

    Welcome to Employee Wage Computation Program

---
+++ src/com/company/EmployeeWageBuilder.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args) {
+
+    System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+}
